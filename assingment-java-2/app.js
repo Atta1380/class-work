63d6dbcfb8718b96fd75ec2af6327f249cba7d27
Muhammad Atta ul Mustafa <120771673+Atta1380@users.noreply.github.com>
2023-01-31 20:54:01 +0500

    Add files via upload

---
+++ assingment-java-2/app.js
@@ -0,0 +1,70 @@
+
+
+//var name = "Atta /n  ul Mustafa";
+
+//name = "ata";
+
+//console.log("Hello")
+
+//document.write("<h4>Muhammad Atta ul Mustafa</h4>")
+
+//var name="Muhammad Atta ul Mustafa"
+
+//name="my-name"
+
+//console.log("my-name")
+
+
+//var name = "username";
+
+//  ****Chepter 2
+//   Question 1,2
+
+var name = 	"myname";
+
+myname= " Muhammad Atta ul Mustafa"
+
+alert(myname)
+
+//var name = 	"massage";
+
+//massage= "Hello World"
+
+//alert(massage)
+
+//   Question 3,4
+var name = 	"age";
+
+age= "27 years old"
+
+alert(age)
+
+var name = 	"corse";
+
+corse= "Certified Web&Mobile Application Develoment"
+
+alert(corse)
+ 
+//  Question 5
+var name = 	"pizza";
+
+pizza= "PIZZA\nPIZZ\nPIZ\nPI\nP"
+
+alert(pizza)
+
+swal("Good job!", "You clicked the OK button!", "success");
+
+ // Question 6
+swal("My Email address is", "attaulmustafa@gmail.com");
+
+//   Question 7
+
+var book = 	"I am trying to learn from the Book \n A Smarter Way to Learn JavaScript";
+alert(book)
+// Question 8
+document.write("<h6>Yah!</h6></br>I can write <b>HTML</b>content through JavaScript")
+//Question 9
+
+var style= "------__****%%#~~~____-------"
+
+alert(style)
